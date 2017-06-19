1f6eab1a222d60cb2c56b5cd4c6bff9dd8b5b662
NizioGames <NizioGaming@gmail.com>
2017-06-19 16:04:29 -0400

    Add project files.

---
+++ variables/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a;
+	double b, c;
+	a = 0;
+	b = 0;
+	cout << "enter a: ";
+	cin >> a;
+	cout << "enter b: ";
+	cin >> b;
+	c = a + b;
+	cout << "The answer is " << c  << endl;
+	system("pause");
+	return 0;
+}
